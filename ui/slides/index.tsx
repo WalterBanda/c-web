0a8bb33ca413add179e1aea70a52920ecaf1659e
Walter Banda <walterkaimurima@gmail.com>
2022-12-21 01:50:19 +0300

    ☕Hero section refactor + Fixed mobile bg

--- ui/slides/index.tsx
+++ ui/slides/index.tsx
@@ -1,12 +1,6 @@
 import styles from './index.module.scss'
+import { motion } from 'framer-motion'
 
-export function Splash() {
-  return (
-    <div className={styles.splash}>
-      <div>
-        ChapChap helps you find reliable mechanics in your area and book
-        appointments with just a few taps
-      </div>
-    </div>
-  )
+export function Hero() {
+  return <motion.div>Hero section</motion.div>
 }
