16295cac1e5f613205cb4667754b59e4325da6ca
Walter Banda <walterkaimurima@gmail.com>
2022-12-20 21:35:14 +0300

    💻 Extracted project meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import Head from 'next/head'
+import Meta from '@ui/meta'
 
 function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'></div>
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <Meta />
+    </div>
   )
 }
 
