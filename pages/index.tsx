cf732d61627c4900e5672886264b524f7a68a460
Walter Banda <walterkaimurima@gmail.com>
2022-12-21 00:11:31 +0300

    ☕Splash page initial UI + Removed app grid for release

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Meta from '@ui/meta'
+import Splash from '@ui/slides/splash'
 
 function Home() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
       <Meta />
+      <Splash />
     </div>
   )
 }
