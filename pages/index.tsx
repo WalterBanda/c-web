0fec4e5aab780e42efb75e3cfa69e146cbdc40f3
Walter Banda <walterkaimurima@gmail.com>
2022-12-21 01:39:40 +0300

    🧹 Reset styles + Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Meta from '@ui/meta'
-import Splash from '@ui/slides/splash'
+import Splash from '@ui/slides'
+import Background from 'components/background'
 
 function Home() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
       <Meta />
       <Splash />
+      <Background />
     </div>
   )
 }
