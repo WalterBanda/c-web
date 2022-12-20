0e23332227becafee25b968a67bccf96d6484df8
Walter Banda <walterkaimurima@gmail.com>
2022-12-20 21:33:28 +0300

    💻 Project library paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,58 +2,7 @@ import Head from 'next/head'
 
 function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <Head>
-        <title>ChapChap</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta
-          name='title'
-          content='ChapChap - Location based mechanical service delivery'
-        />
-        <meta
-          name='description'
-          content='ChapChap helps you find and deliver services to your nearest customer. It aids motorists in locating the nearest service center, ensuring that they are never stranded on the road again.'
-        />
-
-        {/*  Open Graph / Facebook  */}
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://chapchap.murima/' />
-        <meta
-          property='og:title'
-          content='ChapChap - Location based mechanical service delivery'
-        />
-        <meta
-          property='og:description'
-          content='ChapChap helps you find and deliver services to your nearest customer. It aids motorists in locating the nearest service center, ensuring that they are never stranded on the road again.'
-        />
-        <meta
-          property='og:image'
-          content='https://chapchap.murima/marketing/thumbnail.png'
-        />
-
-        {/*Twitter */}
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:url' content='https://chapchap.murima/' />
-        <meta
-          name='twitter:title'
-          content='ChapChap - Location based mechanical service delivery'
-        />
-        <meta
-          name='twitter:description'
-          content='ChapChap helps you find and deliver services to your nearest customer. It aids motorists in locating the nearest service center, ensuring that they are never stranded on the road again.'
-        />
-        <meta
-          name='twitter:image'
-          content='https://chapchap.murima/marketing/thumbnail.png'
-        />
-
-        {/* iOS meta tags & icons */}
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='white' />
-        <meta name='apple-mobile-web-app-title' content='ChapChap' />
-        <link rel='apple-touch-icon' href='icons/apple-touch-icon.png' />
-      </Head>
-    </div>
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'></div>
   )
 }
 
