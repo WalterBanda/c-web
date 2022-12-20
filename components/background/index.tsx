0ea63505197c1709ebb7011db5bdecba11cf6114
Walter Banda <walterkaimurima@gmail.com>
2022-12-21 01:40:59 +0300

    ☕Page Background

---
+++ components/background/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss'
+
+export default function Background() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.content} />
+    </div>
+  )
+}
---
+++ ui/slides/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.scss'
+
+export default function Splash() {
+  return (
+    <div className={styles.root}>
+      <div>
+        ChapChap helps you find reliable mechanics in your area and book
+        appointments with just a few taps
+      </div>
+    </div>
+  )
+}
