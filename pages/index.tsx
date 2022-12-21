d0ae67a516279ae44de844e19d872137319604cd
Walter Banda <walterkaimurima@gmail.com>
2022-12-21 17:21:01 +0300

    ☕Background postioning fix + Components cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
 import Meta from 'components/meta'
-import Background from 'components/background'
 
 function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div>
       <Meta />
-      <Background />
     </div>
   )
-}
-
-function Page() {
-  return <div>
-    
-  </div>
 }
 
 export default Home
