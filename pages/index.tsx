5d8bdb7c0f82cd1bae80d6eb6a2f93a4c7a53536
Walter Banda <walterkaimurima@gmail.com>
2022-12-21 16:14:11 +0300

    ☕Projects structure cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import Meta from '@ui/meta'
-import { Hero } from '@ui/slides'
+import Meta from 'components/meta'
 import Background from 'components/background'
 
 function Home() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
       <Meta />
-      <Hero />
       <Background />
     </div>
   )
--- ui/slides/index.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './index.module.scss'
-import { motion } from 'framer-motion'
-
-export function Hero() {
-  return <motion.div>Hero section</motion.div>
-}
