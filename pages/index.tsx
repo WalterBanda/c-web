7e722b06c5e06f1be3a3597b2aa6b6ef144f5cf5
Walter Banda <walterkaimurima@gmail.com>
2022-12-24 14:43:56 +0300

    💻  Updated background opacity + Inserted Home component into DOM

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Bg from '@components/background'
 import Meta from '@components/meta'
+import Home from '@components/home'
 
 export default function Index() {
   return (
     <>
       <Meta />
+      <Home />
       <Bg />
     </>
   )
