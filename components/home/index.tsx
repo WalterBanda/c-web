38254c7214b60938539e6c30274995d0b0132cdb
Walter Banda <walterkaimurima@gmail.com>
2022-12-30 18:39:28 +0300

    💻  CTA text

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,6 +1,38 @@
+// eslint-disable react/no-unescaped-entities
 import styles from './index.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
 import Link from 'next/link'
+
+function CTA() {
+  return (
+    <div className='w-screen h-screen relative'>
+      <h1>Finding a good mechanic is hard.</h1>
+      <div>
+        <p>
+          It's stressful, time-consuming, and sometimes downright impossible.
+        </p>
+        <p>
+          But don't worry - we've got you covered! Chapchap makes it easy to
+          find top-rated mechanics in your area.
+        </p>
+      </div>
+      <div>
+        <p>
+          Simply download the app, create your account and tada you connect with
+          great mechanics in your location.
+        </p>
+        <p>
+          If you are a mechanic, download the app and register as a garage to
+          get your services discovered.
+        </p>
+      </div>
+      <div>
+        <p>And great news, its Opensource</p>
+        <p>Get it now at</p>
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -11,6 +43,7 @@ export default function Home() {
       >
         <VscGithubInverted />
       </Link>
+      <CTA />
     </main>
   )
 }
