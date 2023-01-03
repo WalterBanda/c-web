fb0a3fac37387983d8b7e6c59347f3ee7dd58b70
Walter Banda <walterkaimurima@gmail.com>
2023-01-03 12:47:02 +0300

    ☕CTA text styling + hero img sizing styles

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 function Slides() {
   return (
     <div className='w-full h-full'>
-      <h1>Finding a good mechanic is hard.</h1>
+      <h1 className={styles.splash_hero}>Finding a good mechanic is hard.</h1>
       <div className={styles.splash}>
         <Image src='/marketing/splash.png' alt='Splash page for app' fill />
       </div>
