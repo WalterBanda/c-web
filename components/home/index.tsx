7ad75dd776ad2465917e29d46427a9307f4164d3
Walter Banda <walterkaimurima@gmail.com>
2023-01-03 12:51:50 +0300

    ☕CLearing structure

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,18 +1,6 @@
 import styles from './index.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
 import Link from 'next/link'
-import Image from 'next/image'
-
-function Slides() {
-  return (
-    <div className='w-full h-full'>
-      <h1 className={styles.splash_hero}>Finding a good mechanic is hard.</h1>
-      <div className={styles.splash}>
-        <Image src='/marketing/splash.png' alt='Splash page for app' fill />
-      </div>
-    </div>
-  )
-}
 
 export default function Home() {
   return (
@@ -23,7 +11,6 @@ export default function Home() {
       >
         <VscGithubInverted />
       </Link>
-      <Slides />
     </main>
   )
 }
